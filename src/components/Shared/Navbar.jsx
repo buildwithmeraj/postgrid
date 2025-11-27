95aa5793c7ac3fd7c8c70654ff529b4b441b5a7d
Meraj <buildwithmeraj@gmail.com>
2025-11-27 09:48:36 +0600

    fixed profile dropdown position in navbar

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -80,7 +80,7 @@ async function Navbar() {
       </div>
       <div className="navbar-end">
         {user ? (
-          <div className="dropdown dropdown-center hidden md:flex">
+          <div className="dropdown dropdown-center">
             <div tabIndex={0} role="button" className="avatar cursor-pointer">
               <div className="w-10 rounded-full">
                 <img
